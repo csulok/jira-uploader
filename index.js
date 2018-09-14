773a227ec5d430091cdc885b52a88fbaa2c142c4
Andras Stracz <astracz@chemaxon.com>
2018-09-14 17:25:32 +0200

    feat: next stuff

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express")
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port 80!'))
+
+const corsOptions = {
+  origin: "*"
+};
+app.options("/api", cors(corsOptions));
+app.post("/api", cors(corsOptions));
+
+app.listen(process.env.PORT, () => console.log("Example app listening on port 80!"))
+
+app.get("/", (req, res) => res.send("It works!"));
+app.post("/", bodyParser.json(), (req, res) => {
+  const issueId = req.body.ideaId;
+  const canvas = req.body.canvas;
+  console.log(issueId, canvas);
+});
