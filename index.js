a5763e5c4f28e8b38a0ef4a76f0f2f3d7e758570
Andras Stracz <astracz@chemaxon.com>
2018-09-15 19:18:25 +0200

    feat: add jira uploading

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const request =  require("request-promise");
 
 const app = express();
 
 
 const corsOptions = {
-  origin: ["https://hypo-drawing.chemaxon.com", "http://localhost", "*"],
+  origin: ["https://hypo-drawing.chemaxon.com"],
   methods: ["GET", "HEAD", "POST", "OPTIONS"]
 };
 app.options("/api", cors(corsOptions));
@@ -14,9 +15,43 @@ app.post("/api", cors(corsOptions));
 
 app.listen(process.env.PORT, () => console.log("Example app listening on port 80!"))
 
-app.get("/", (req, res) => res.send("It works!"));
-app.post("/api", bodyParser.json(), (req, res) => {
+app.get("/", (req, res) => res.send("Available endpoints: POST /api"));
+app.get("/status", (req, res) => res.json({ status: "OK"}));
+
+app.post("/api", bodyParser.json(), (req, res, next) => {
   const issueId = req.body.ideaId;
-  const canvas = req.body.canvas;
-  console.log(issueId, canvas);
+  const dataUrl = req.body.dataUrl;
+  const fileName = req.body.fileName;
+  console.log(issueId, fileName, "dataUrl length:", dataUrl.length);
+  
+  if (!ideaId || !dataUrl || !fileName) {
+    return next();
+  }
+  console.log("using", process.env["JIRA_USERNAME"], process.env["JIRA_PASSWORD"]);
+  
+  try {
+    const results = await request.post({
+      url: `https://hypo.chemaxon.com/rest/api/2/issue/${issueId}/attachments`,
+      auth: {
+        username: process.env["JIRA_USERNAME"],
+        password: process.env["JIRA_PASSWORD"]
+      },
+      headers: {
+        "X-Atlassian-Token": "no-check"
+      },
+      form: {
+        value: new Buffer(dataUrl.split(",")[1], 'base64'),
+        options: {
+          filename: fileName,
+          contentType: "image/png"
+        } 
+      }
+    });
+    
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+
 });
